52d6ea880d85e8e5029777dc4c819bcc990fc8b6
josephmtaylor <joseph.taylor@dodatllc.com>
2016-05-12 17:20:31 -0400

    test

--- newdoctor.php
+++ newdoctor.php
@@ -218,6 +218,48 @@ if (isset($_GET["id"]) && !empty($_GET["id"]))
 				
 		
 }
+else if(isset($GET_["id"]) && isset($GET_["msg"]) && $GET_["msg"]=="erre")
+{
+	$id =	$_GET["id"];
+	$fl =	$_GET["fl"];
+	$ctype = $_GET['ctype'];
+	$pg = $_GET['pg'];
+
+		$data  = $M->getRecordByField('doctor_id', $id);
+				
+				
+				if (!empty($data))
+				{	
+					
+					  	$id						=		$data['doctor_id'];
+					  	$first_name				=		$data['first_name'];
+						$middle_name			=		$data['middle_name'];
+						$last_name				=		$data['last_name'];
+						$fullname				=		$data['fullname'];
+						$notes					=		$data['notes'];
+//						$address_1				=		$data['address_1'];
+//						$address_2				=		$data['address_2'];
+//						$city					=		$data['city'];
+//						$state					=		$data['state'];
+//						$zip					=		$data['zip'];
+						
+						$category_id_fk			=		$data['category_id_fk'];
+						$specialty_id_fk		=		$data['specialty_id_fk'];
+						$county_id_fk			=		$data['county_id_fk'];
+						$email					=		$data['email'];
+						$license_status	 	    = 		$data['license_status'];
+						$phone			 	    = 		$data['phone'];
+						$fax			 	    = 		$data['fax'];
+						$website		 	    = 		$data['website'];
+						$npi			 	    = 		$data['npi'];
+						//$publication	 	    = 		$data['publication'];
+						$status					=		$data['status'];
+						
+						$daRec = $DA->getDoctorAddressRecord($fieldName='doctor_id', $id);
+						
+					
+				}
+}
 else {
 		  $fl = "add";
 		  $id = "";
